67d24ad2c032ce4519b32e035e1b173768d80d80
rumor1337 <rumorpersonal@gmail.com>
2026-01-20 12:50:17 +0200

    refactor: remove unused dependencies

--- app.ts
+++ app.ts
@@ -1,10 +1,8 @@
 import Express from './services/express/Express.ts';
-import SortUtil from './util/SortUtil.ts';
 
 class app {
 
     public express = new Express(8080);
-    public sortUtil = new SortUtil();
 
     constructor() {
         this.express.setRoutes();
